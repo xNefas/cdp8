3b63503e9cd45eeae7acfcc4f88d7c2edcdac6b8
xNefas <199727225+xNefas@users.noreply.github.com>
2025-08-20 00:40:26 +0200

    some updates

--- cdp8.nix
+++ cdp8.nix
@@ -4,53 +4,55 @@
   cmake,
   pkg-config,
   fetchFromGitHub,
-  libaaio ? ((import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/32f313e49e42f715491e1ea7b306a87c16fe0388.tar.gz") {}).callPackage ./libaaio.nix {}),
+  libaaio ? ((import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/32f313e49e42f715491e1ea7b306a87c16fe0388.tar.gz";
+      sha256 = "sha256:1z4ga87qla5300qwib3dnjnkaywwh8y1qqsb8w2mrsrw78k9xmlw";
+    }) {}).callPackage
+    ./libaaio.nix {}),
   ...
-}: let
-  cdp8 = fetchFromGitHub {
+}:
+stdenv.mkDerivation {
+  pname = "cdp8";
+  version = "8.0.0";
+
+  src = fetchFromGitHub {
     owner = "ComposersDesktop";
     repo = "CDP8";
     rev = "CDP8.0";
     hash = "sha256-L0ncIcB0OardDykhNHwJ3ae09Sh4iNOQSZRgNV7ZPQ";
   };
-in
-  stdenv.mkDerivation {
-    pname = "cdp8";
-    version = "8.0.0";
 
-    src = cdp8;
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+  ];
 
-    nativeBuildInputs = [
-      pkg-config
-      cmake
-    ];
+  postUnpack = ''
+    # Extract libaaio source into libaaio subdirectory
+    tar xzf ${libaaio.src} -C $sourceRoot/libaaio --strip-components=1
+  '';
 
-    postUnpack = ''
-      # Extract libaaio source into libaaio subdirectory
-      tar xzf ${libaaio.src} -C $sourceRoot/libaaio --strip-components=1
-    '';
+  cmakeFlags = [
+    "-DAAIOLIB=${libaaio}/lib/libaaio.so"
+  ];
 
-    cmakeFlags = [
-      "-DAAIOLIB=${libaaio}/lib/libaaio.so"
-    ];
+  cmakeBuildDir = "source/build";
 
-    cmakeBuildDir = "source/build";
+  installPhase = ''
+    runHook preInstall
 
-    installPhase = ''
-      runHook preInstall
+    # Install CDP8 binaries
+    mkdir -p $out/bin
+    cp -t $out/bin source/build/Release/*
 
-      # Install CDP8 binaries
-      mkdir -p $out/bin
-      cp -t $out/bin source/build/Release/*
+    runHook postInstall
+  '';
 
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "CDP8 audio processing toolkit";
-      maintainers = with maintainers; [enkarterisi];
-      license = licenses.lgpl21Only;
-      platforms = platforms.linux;
-      mainProgram = "cdp8";
-    };
-  }
+  meta = with lib; {
+    description = "CDP8 audio processing toolkit";
+    maintainers = with maintainers; [enkarterisi];
+    license = licenses.lgpl21Only;
+    platforms = platforms.linux;
+    mainProgram = "cdp8";
+  };
+}
