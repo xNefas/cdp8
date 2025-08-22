eff38402f66b4b22e5c4529f40b780d9d3c30956
xNefas <199727225+xNefas@users.noreply.github.com>
2025-08-22 16:17:29 +0200

    cdp8: fix build with libcxxStdenv and portaudio source integration

--- cdp8.nix
+++ cdp8.nix
@@ -1,30 +1,38 @@
-{ lib, stdenv, cmake, pkg-config, fetchFromGitHub, libaaio, portaudio, alsa-lib
-, jack2, }:
-stdenv.mkDerivation (self: {
+{ lib, libcxxStdenv, cmake, pkg-config, fetchFromGitHub, libaaio, portaudio
+, alsa-lib, jack2, autoPatchelfHook }:
+let
+  portaudioWithAlsaJack = portaudio.overrideAttrs (old: {
+    configureFlags = old.configureFlags ++ [ "--with-alsa" "--with-jack" ];
+    dontDisableStatic = true;
+    outputs = [ "out" "dev" ];
+  });
+in libcxxStdenv.mkDerivation (self: {
   pname = "cdp8";
   version = "8.0";
 
   src = fetchFromGitHub {
     owner = "ComposersDesktop";
     repo = "CDP8";
-    rev = "CDP${self.version}";
-    hash = "sha256-/L0ncIcB0OardDykhNHwJ3ae09Sh4iNOQSZRgNV7ZPQ=";
+    rev = "11cdcb4a6742f7ada1ded43e436a77a94d7c4ee4";
+    hash = "sha256-zIwwnUqssU0hHN/6PlxtLcXF9YuZTfHkm61vpbUJ7Ko=";
   };
 
-  nativeBuildInputs = [ cmake libaaio pkg-config ];
+  nativeBuildInputs = [ cmake libaaio pkg-config autoPatchelfHook ];
 
-  buildInputs = [
-    (portaudio.overrideAttrs (old: {
-      configureFlags = old.configureFlags
-        ++ [ "--enable-static" "--with-alsa" "--with-jack" ];
-    }))
-    alsa-lib
-    jack2
-  ];
+  buildInputs = [ portaudioWithAlsaJack alsa-lib jack2 ];
+
+  # Extract PortAudio source into CDP8 source tree,
+  # as per building instructions found here:
+  # https://github.com/ComposersDesktop/CDP8/blob/11cdcb4a6742f7ada1ded43e436a77a94d7c4ee4/dev/externals/paprogs/palinuxbuild.txt
+  preConfigure = ''
+    mkdir -p dev/externals
+    tar -xf ${portaudioWithAlsaJack.src} -C dev/externals/ --strip-components=1
+  '';
 
   cmakeFlags = [
     "-DAAIOLIB=${lib.getLib libaaio}/lib/libaaio.so"
-    "-DPORTAUDIOLIB=${portaudio}/lib/libportaudio.a" # CDP expects static library
+    "-DPORTAUDIO_LIBRARY=${portaudioWithAlsaJack}/lib/libportaudio.so"
+    "-DCMAKE_SKIP_RPATH=ON"
   ];
 
   meta = {
--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   description = "Test cdp8 derivation";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/32f313e49e42f715491e1ea7b306a87c16fe0388";
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/32f313e49e42f715491e1ea7b306a87c16fe0388";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: {
-    packages.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; {
-      libaaio = callPackage ./libaaio.nix {};
-      default = callPackage ./cdp8.nix {inherit (self.packages.${system}) libaaio;};
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system} = {
+        libaaio = pkgs.callPackage ./libaaio.nix { };
+        default = pkgs.callPackage ./cdp8.nix {
+          inherit (self.packages.${system}) libaaio;
+        };
+      };
     };
-  };
 }
